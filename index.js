ee9a28d885cf0c09fe09ee6fa4a16a10e788b55e
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2022-06-22 00:23:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const PROFILE = {
+  name: "Okwor Chuukwuemeka Nnajiofor",
+  height: "185cm",
+  country: "Nigeria",
+};
+
+console.log(
+  `My name is ${PROFILE.name}, I am ${PROFILE.height} tall and I am from ${PROFILE.country}`
+);
